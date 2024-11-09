04e8fd0f4cfee81337ca883ee1b1ba3c50298503
Dreams Obasohan <jtp7d03u@students.codeinstitute.net>
2024-11-09 00:20:44 +0000

    add script codes start button and answer button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,45 @@
 const startButton = document.getElementById("start-btn")
 const questionContainerElement = document.getElementById("question-container")
+const questionElement = document.getElementById("question")
+const answerButtonsElement = document.getElementById("answer-buttons")
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener("click", startGame)
 
 
-function startGame(){
+function startGame() {
     console.log("Started")
     startButton.classList.add("hide")
+    questionContainerElement.classList.remove("hide")
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
-    questionContainerElement.classList.remove("hide")
     setNextQuestion()
 
 }
 
-function setNextQuestion(){
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 
 
 }
 
-function selectAnswer(){
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+
+function selectAnswer(e) {
 
 
 }
@@ -28,10 +48,10 @@ function selectAnswer(){
 
 const questions = [
     {
-    question: "What is 2 + 2?",
-    answers: [
-    {text: "4", correct: true},
-    {text: "22", correct: false}
-    ]
-    },
-    
+        question: "What is 2 + 2 ?",
+        answers: [
+            { text: "4", correct: true },
+            { text: "22", correct: false }
+        ]
+    }
+]

ce4e3d7d1a2ff08bc4903d3805e3d5f5d90e85d1
Dreams Obasohan <jtp7d03u@students.codeinstitute.net>
2024-11-09 01:35:04 +0000

    add code for restart button and and next question

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,10 @@ const answerButtonsElement = document.getElementById("answer-buttons")
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener("click", startGame)
-
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
     startButton.classList.add("hide")
@@ -45,28 +48,34 @@ function resetState() {
     }
 }
 
-    function selectAnswer(e){
-        const selectedButton = e.target
-        const correct = selectedButton.dataset.correct
-        setStatusClass(document.body, correct)
-        Array.from(answerButtonsElement.children).forEach(button => {
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
-        })
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove("hide")
+    } else {
+        startButton.innerText = "Restart"
+        startButton.classList.remove("hide")
+    }
 }
 
-function setStatusClass(element, correct){
+function setStatusClass(element, correct) {
     clearStatusClass(element)
     if (correct) {
-    element.classList.add("correct")
-    }else{
-    element.classList.add("wrong")
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
     }
 }
 
-function clearStatusClass(element){
+function clearStatusClass(element) {
     element.classList.remove("correct")
     element.classList.remove("wrong")
-    }
+}
 
 const questions = [
     {
@@ -75,5 +84,5 @@ const questions = [
             { text: "4", correct: true },
             { text: "22", correct: false }
         ]
-    }
+    },
 ]

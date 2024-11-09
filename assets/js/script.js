4fb7cff85410e089958758dd2729ca3364c25ddf
Dreams Obasohan <jtp7d03u@students.codeinstitute.net>
2024-11-09 00:40:10 +0000

    add codes for status class and clear status in quiz game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById("start-btn")
+const nextButton = document.getElementById("next-btn")
 const questionContainerElement = document.getElementById("question-container")
 const questionElement = document.getElementById("question")
 const answerButtonsElement = document.getElementById("answer-buttons")
@@ -8,7 +9,6 @@ startButton.addEventListener("click", startGame)
 
 
 function startGame() {
-    console.log("Started")
     startButton.classList.add("hide")
     questionContainerElement.classList.remove("hide")
     shuffledQuestions = questions.sort(() => Math.random() - .5)
@@ -20,7 +20,6 @@ function startGame() {
 function setNextQuestion() {
     resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
-
 
 }
 
@@ -38,13 +37,36 @@ function showQuestion(question) {
     })
 }
 
-
-function selectAnswer(e) {
-
-
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add("hide")
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
 }
 
+    function selectAnswer(e){
+        const selectedButton = e.target
+        const correct = selectedButton.dataset.correct
+        setStatusClass(document.body, correct)
+        Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+        })
+}
 
+function setStatusClass(element, correct){
+    clearStatusClass(element)
+    if (correct) {
+    element.classList.add("correct")
+    }else{
+    element.classList.add("wrong")
+    }
+}
+
+function clearStatusClass(element){
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
+    }
 
 const questions = [
     {
